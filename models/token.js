9f62903aa37bba465e589d8afb14d262d72c548a
Adamo  kehinde hassan <102253825+lambojac@users.noreply.github.com>
2023-09-08 13:05:37 +0100

    user tokens

--- models/token.js
+++ models/token.js
@@ -1,1 +1,32 @@
+const mongoose = require('mongoose');
+
+const tokenSchema = new mongoose.Schema({
+    userId: {
+        type: Number,
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    tokenCode: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['used', 'unused'],
+    },
+}, {
+    timestamps: true, 
+});
+
+const Token = mongoose.model('Token', tokenSchema);
+
+module.exports = Token;
 
