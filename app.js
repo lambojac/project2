5f59c0c893819a84d180474969948130cdb518c3
Adamo  kehinde hassan <102253825+lambojac@users.noreply.github.com>
2023-09-08 14:38:21 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from 'morgan';
+import cors from "cors";
+import mongoose from 'mongoose';
+import bodyParser from "body-parser";
+
+dotenv.config();
+
+const app = express();
+
+app.use(morgan('dev')); 
+app.use(cors()); 
+app.use(express.static('build')); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit(0);
+  });
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
